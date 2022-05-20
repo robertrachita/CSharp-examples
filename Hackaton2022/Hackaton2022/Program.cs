e9c6d0b92adc3089aff25a10ae7b6e568d86d536
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2022-05-20 10:40:38 +0200

    filewriter
    working
    plus example in main class

--- Hackaton2022/Hackaton2022/Program.cs
+++ Hackaton2022/Hackaton2022/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Hackaton2022;
+
+
+string path = "@..\\..\\..\\..\\..\\test.txt";
+String[] strings = FileReader.ReadFileLines(path);
+
+//FileReader.WriteToConsole(strings);
+
+FileWriter.WriteNewLine(path);
+FileWriter.Append(path, "yes");
+
+FileReader.WriteToConsole(FileReader.ReadFile(path));
+
+
+Console.WriteLine("Press any key to exit.");
+System.Console.ReadKey();
