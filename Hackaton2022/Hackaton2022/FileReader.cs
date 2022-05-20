107ed404e44942e53a8247c60ab7403b76a97276
Robert Rachita <72192997+robertrachita@users.noreply.github.com>
2022-05-20 01:41:12 -0700

    Developing (#1)
    * init
    
    * file reader
    
    * filewriter
    
    working
    plus example in main class

---
+++ Hackaton2022/Hackaton2022/FileReader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackaton2022
+{
+    public static class FileReader
+    {
+        static FileReader()
+        {
+        }
+
+        public static string ReadFile(string fileName)
+        {
+            return System.IO.File.ReadAllText(fileName);
+        }
+
+        public static string[] ReadFileLines(string fileName)
+        {
+            return System.IO.File.ReadAllLines(fileName);
+        }
+
+
+        public static bool FileExists(string fileName)
+        {
+            return System.IO.File.Exists(fileName);
+        }
+
+        public static void CreateDirectory(string directoryName)
+        {
+            System.IO.Directory.CreateDirectory(directoryName);
+        }
+
+        public static void WriteToConsole(string[] lines)
+        {
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        public static void WriteToConsole(string lines)
+        {
+
+            Console.WriteLine(lines);
+        }
+
+        public static void WriteNewLineToConsole()
+        {
+            Console.WriteLine();
+        }
+
+    }
+
+}
---
+++ Hackaton2022/Hackaton2022/FileWriter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackaton2022
+{
+    public static class FileWriter
+    {
+        public static async Task WriteArray(string fileName, string[] content)
+        {
+            await File.WriteAllLinesAsync(fileName, content);
+        }
+
+        public static async Task WriteLine(string fileName, string content)
+        {
+            await File.WriteAllTextAsync(fileName, content);
+        }
+
+        public static async Task NewAppend(string fileName, string content)
+        {
+            await File.AppendAllTextAsync(fileName, content);
+        }
+
+        public static async Task AppendArray(string fileName, string[] content)
+        {
+            foreach (string line in content)
+            {
+                await File.AppendAllTextAsync(fileName, line);
+            }
+        }
+
+        public static async Task WriteNewLine(string fileName)
+        {
+            using StreamWriter file = new(fileName, append: true);
+            await file.WriteLineAsync("\n");
+        }
+        
+        public static async Task Append(string fileName, string content)
+        {
+            using StreamWriter file = new(fileName, append: true);
+            await file.WriteLineAsync(content);
+        }
+
+        public static async Task Append(string fileName, string[] content)
+        {
+            using StreamWriter file = new(fileName, append: true);
+            foreach(string line in content)
+            {
+                await file.WriteLineAsync(line);
+            }
+        }
+
+    }
+}
